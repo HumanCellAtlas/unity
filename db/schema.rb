4d1f9537d8cac731bbaa37e31e56544e088a3941
bistline <bistline@broadinstitute.org>
2018-08-16 14:03:52 -0400

    adding parameter_type to reference_analysis_data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_16_165202) do
+ActiveRecord::Schema.define(version: 2018_08_16_180025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 2018_08_16_165202) do
     t.datetime "updated_at", null: false
     t.string "data_type"
     t.string "call_name"
+    t.string "parameter_type"
   end
 
   create_table "reference_analysis_options", force: :cascade do |t|
